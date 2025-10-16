275bb3abcaa769fd207409795631a001b43e60ab
scmeek <sean@seanmeek.com>
2025-10-16 16:59:14 -0600

    functions.sh - add print handling for "note"

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,11 +1,16 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+BLUE='\033[1;34m'
 MAGENTA='\033[0;35m'
 NC='\033[0m' # No Color
 
 info() {
   echo "${YELLOW}==> $1${NC}"
+}
+
+note() {
+  echo "${BLUE}ℹ️ ${NC}"
 }
 
 warn() {
