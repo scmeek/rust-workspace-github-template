fd24b31d50dc25b3f084c407d527fa34577abb39
scmeek <sean@seanmeek.com>
2025-10-17 00:26:30 -0600

    Remove cargo-tarpaulin dependency for now until code coverage is added

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,6 +7,6 @@ PROJECT_ROOT="${PROJECT_ROOT:-$(CDPATH='' cd -- "$SCRIPTS_DIR/.." && pwd)}"
 
 . "${SCRIPTS_DIR}/functions.sh"
 
-cargo install cargo-tarpaulin --locked
 cargo install cargo-workspace-lints
+# cargo install cargo-tarpaulin --locked
 cargo install cargo-udeps --locked
