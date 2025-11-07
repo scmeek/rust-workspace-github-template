76322e9733e98153d87b966411e8c885decd5365
scmeek <sean@seanmeek.com>
2025-11-07 10:50:32 -0700

    Add SECURITY.md and add cargo-audit Make recipe and CI pipeline

---
+++ scripts/audit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+SCRIPTS_DIR="${SCRIPTS_DIR:-$(dirname -- "$(readlink -f -- "$0")")}"
+PROJECT_ROOT="${PROJECT_ROOT:-$(CDPATH='' cd -- "$SCRIPTS_DIR/.." && pwd)}"
+
+. "${SCRIPTS_DIR}/functions.sh"
+
+echo ""
+
+info "Running dependencies audit..."
+
+AUDIT_CMD="cargo audit"
+if ! $AUDIT_CMD; then
+  fail "Dependencies audit failed.. Run \`$AUDIT_CMD\` and fix issues."
+fi
+success "Dependencies passed audit."
