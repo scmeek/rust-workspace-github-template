460060fd523b822fa8992cf5e4d6118dbe4308b3
scmeek <sean@seanmeek.com>
2025-11-08 08:23:49 -0700

    fix: Use caching for test.yml

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -10,9 +10,9 @@ PROJECT_ROOT="${PROJECT_ROOT:-$(CDPATH='' cd -- "$SCRIPTS_DIR/.." && pwd)}"
 # Install llvm-tools-preview (required for llvm-cov)
 rustup component add llvm-tools-preview
 
-cargo install --locked cargo-audit # Also in .github/workflows/audit.yml
-cargo install --locked cargo-llvm-cov
-cargo install --locked cargo-nextest
+cargo install --locked cargo-audit    # Also in .github/workflows/audit.yml
+cargo install --locked cargo-llvm-cov # Also in .github/workflows/test.yml
+cargo install --locked cargo-nextest  # Also in .github/workflows/test.yml
 cargo install --locked cargo-semver-checks
 cargo install --locked cargo-udeps           # Also in .github/workflows/dependencies.yml
 cargo install --locked cargo-workspace-lints # Also in .github/workflows/dependencies.sh
