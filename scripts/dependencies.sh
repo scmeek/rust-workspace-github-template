79bfeed1ac74e3881d798e215dc553bc6ddc898f
scmeek <sean@seanmeek.com>
2025-11-08 08:16:39 -0700

    fix: Use caching for audit.yml workflow

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -10,7 +10,7 @@ PROJECT_ROOT="${PROJECT_ROOT:-$(CDPATH='' cd -- "$SCRIPTS_DIR/.." && pwd)}"
 # Install llvm-tools-preview (required for llvm-cov)
 rustup component add llvm-tools-preview
 
-cargo install --locked cargo-audit
+cargo install --locked cargo-audit # Also in .github/workflows/audit.yml
 cargo install --locked cargo-llvm-cov
 cargo install --locked cargo-nextest
 cargo install --locked cargo-semver-checks
