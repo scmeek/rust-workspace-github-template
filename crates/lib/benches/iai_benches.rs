edccef7a572aeb20125d75dfde27e1626d1f13b0
scmeek <sean@seanmeek.com>
2025-11-09 17:28:27 -0700

    fix: Attempt fix of benches for test

--- crates/lib/benches/iai_benches.rs
+++ crates/lib/benches/iai_benches.rs
@@ -1,19 +1,17 @@
-fn main() {
-    #[cfg(target_os = "linux")]
-    {
-        use iai_callgrind::{library_benchmark, library_benchmark_group, main};
-        use lib::add;
-        use std::hint::black_box;
+#[cfg(target_os = "linux")]
+mod linux_benches {
+    use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+    use lib::add;
+    use std::hint::black_box;
 
-        #[library_benchmark]
-        fn iai_add() {
-            black_box(add(black_box(42), black_box(58)));
-        }
-
-        library_benchmark_group!(name = my_group; benchmarks = iai_my_function);
-        main!(library_benchmark_groups = my_group);
+    #[library_benchmark]
+    fn iai_add() {
+        black_box(add(black_box(42), black_box(58)));
     }
 
-    #[cfg(not(target_os = "linux"))] // No-op
-    {}
+    library_benchmark_group!(name = my_group; benchmarks = iai_my_function);
+    main!(library_benchmark_groups = my_group);
 }
+
+#[cfg(not(target_os = "linux"))] // No-op
+fn main() {}
