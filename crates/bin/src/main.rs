1657d8f5cca85384f07d86959e93de747b1cea6f
scmeek <sean@seanmeek.com>
2025-11-09 18:34:49 -0700

    test: Add test for bin::main()

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -6,3 +6,13 @@ fn main() {
     println!("Hello, world!");
     println!("{}", add(1, 2));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn main_works() {
+        main();
+    }
+}
