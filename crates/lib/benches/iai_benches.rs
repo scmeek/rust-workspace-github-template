8437fa6db4a84a6c0670d1fb494a1a087acb6f38
scmeek <sean@seanmeek.com>
2025-11-09 17:17:01 -0700

    fix: Attempt fix of iai_benches for test

--- crates/lib/benches/iai_benches.rs
+++ crates/lib/benches/iai_benches.rs
@@ -1,19 +1,19 @@
-#![cfg(target_os = "linux")]
-#![allow(clippy::exit, reason = "Use of main macro by Iai-Callgrind")]
+fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+        use lib::add;
+        use std::hint::black_box;
 
-use iai_callgrind::{library_benchmark, library_benchmark_group, main};
-use lib::add;
-use std::hint::black_box;
+        #[library_benchmark]
+        fn iai_add() {
+            black_box(add(black_box(42), black_box(58)));
+        }
 
-#[library_benchmark]
-fn iai_add() {
-    black_box(add(black_box(42), black_box(58)));
+        library_benchmark_group!(name = my_group; benchmarks = iai_my_function);
+        main!(library_benchmark_groups = my_group);
+    }
+
+    #[cfg(not(target_os = "linux"))] // No-op
+    {}
 }
-
-library_benchmark_group!(
-    name = my_group;
-    benchmarks =
-        iai_add,
-);
-
-main!(library_benchmark_groups = my_group);
